1c194c7ed7d1179a33af87ac2878e21cc6d8f66e
Lopatin E.A <i@lopatin-ea.ru>
2023-06-22 02:04:30 +0700

    add hello USER console project

---
+++ examp002_hellouser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! What is your name?");
+string username = Console.ReadLine();
+Console.Write("Nice to meet you, ");
+Console.Write(username);
--- example001_helloConsole/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2 Hello, World!");
-Console.WriteLine("dotnet new console - создать новый проект");
-Console.WriteLine("dotnet run - запусьтить проект");
-Console.WriteLine("Через console мы обращаемся к систомному терминалу");
