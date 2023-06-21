f2d3d60b40c2a93064d5aed27a2cda850674dbf4
Lopatin E.A <i@lopatin-ea.ru>
2023-06-22 02:25:34 +0700

    add hello USER commands

--- examp002_hellouser/Program.cs
+++ examp002_hellouser/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// WriteLine — вывод в консоль с переходом на новую строку.
+// Write — это вывод в консоль без перехода на новую строку. 
+// ReadLine — позволяет считывать данные из нашего терминала.
+
+
 Console.WriteLine("Hi! What is your name?");
 string username = Console.ReadLine();
 Console.Write("Nice to meet you, ");
 Console.Write(username);
+
