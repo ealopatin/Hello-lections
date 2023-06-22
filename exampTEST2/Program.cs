3ec87fb35c4946a4f9617d8893dd14d857732de9
Lopatin E.A <i@lopatin-ea.ru>
2023-06-22 23:41:02 +0700

    dotnet 6version

---
+++ exampTEST2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя: ");
+string userInput = Console.ReadLine() ?? "";
+
+if (userInput == "Витя")
+    Console.Write("здарова Витька");
+else
+    Console.Write("всем рахмет");
+
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA+numberB;
+Console.WriteLine(result);
