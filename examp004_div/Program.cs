181802c44a45c717afd3f0c2cc18c27ad3174197
Lopatin E.A <i@lopatin-ea.ru>
2023-06-22 20:30:44 +0700

    ex4 dev

--- examp004_div/Program.cs
+++ examp004_div/Program.cs
@@ -1,3 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10); // 1 2 3 4 5 5 6 7 8 9 lftn ckexfqyjt xbckj [min, max -1] или [min, max)
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
 Console.WriteLine(numberA / numberB);
