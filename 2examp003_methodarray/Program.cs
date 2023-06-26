a843e51f1e10e564638e5cffa34d01e5c02891da
Lopatin E.A <i@lopatin-ea.ru>
2023-06-26 14:31:36 +0700

    add comment lection2

---
+++ 2examp003_methodarray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {11,21,31,4,51,61,999,61,91};
+
+int n = array.Length;
+int find = 61;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ 2examp004_ArrayLibriary/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+            collection[index] = new Random().Next(1, 100);
+            index++;
+    }
+}
+
+void PrintArray(int[] collection2)
+{
+    int count = collection2.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(collection2[position]);
+        position++;
+    }
+
+}   
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+
+//с void не исользуем return
