629c764137845a0606dba8b40eaf1efee20d3ae6
Lopatin E.A <i@lopatin-ea.ru>
2023-07-02 23:25:27 +0700

    add lection 3 examples + lect homework

--- 2examp004_ArrayLibriary/Program.cs
+++ 2examp004_ArrayLibriary/Program.cs
@@ -15,36 +15,37 @@ void PrintArray(int[] collection2)
     int position = 0;
     while (position < count)
     {
-        Console.WriteLine(collection2[position]);
+        Console.Write(collection2[position]);
         position++;
     }
 
 }
 
-int IndexOf(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
+// int IndexOf(int[] collection, int find)
+// {
+//     int count = collection.Length;
+//     int index = 0;
+//     int position = -1;
 
-    while (index < count)
-    {
-        if (collection[index] == find)
-        {
-            position = index;
-            break;
-        }
-        index++;
-    }
-    return position;
-}
+//     while (index < count)
+//     {
+//         if (collection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
+//     }
+//     return position;
+// }
 
 int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf(array, 4);
-Console.WriteLine(pos);
+// int pos = IndexOf(array, 4);
+// Console.Write(pos);
 
 //с void не исользуем return
+
